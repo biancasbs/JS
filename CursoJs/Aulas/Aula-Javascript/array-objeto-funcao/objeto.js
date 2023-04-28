1bdb646a6bacca065c486ff46ce6bbd440c7536b
Bianca Sousa <75484555+biancasbs@users.noreply.github.com>
2023-04-28 12:20:45 -0300

    aula21

---
+++ CursoJs/Aulas/Aula-Javascript/array-objeto-funcao/objeto.js
@@ -0,0 +1,10 @@
+function criaPessoa(nome, sobrenome, idade) {
+    return {
+        nome: nome, //pode deixar só uma linha com nome
+        sobrenome: sobrenome,
+        idade: idade,
+    };
+};
+
+console.log('luiz', 'otavio', 25);
+
---
+++ CursoJs/Aulas/Aula-Javascript/array-objeto-funcao/operadores.js
@@ -0,0 +1,24 @@
+/*
+operadores numericos utilizados:
+> maior que 
+< menor que 
+>= maior igual a
+=< menor igual a
+=== igual em valor e operação
+!== diferente em valor e operação
+
+*/
+/*
+Operadores logicos
+&& - and - todas precisam ser verdadeiras
+|| - or - ou uma ou outra precisam ser verdadeiras. se todas forem falsas todas vao retornar false
+! - not - não verdadeira ex. !true = false
+
+*/
+/* FALSY 
+* false - literal
+NaN 
+'' "" ``
+0
+undefined/null
+*/
