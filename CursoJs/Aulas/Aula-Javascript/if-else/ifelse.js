788498f806baa49d321c2e1b9dca698fdd255d8e
Bianca Sousa <75484555+biancasbs@users.noreply.github.com>
2023-05-02 18:13:16 -0300

    exercicios
    aula 22

---
+++ CursoJs/Aulas/Aula-Javascript/if-else/ifelse.js
@@ -0,0 +1,13 @@
+const hora = 12;
+
+if (hora >= 0 && hora <= 12){
+    console.log('Bom dia!');
+} else if (hora <= 18) {
+    console.log('Boa tarde!');
+} else if (hora <=24) {
+    console.log ('Boa noite!');
+} else {
+    console.log('Olá');
+}
+
+
